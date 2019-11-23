7fcc3607f028b875fb7991c9760b7ba63061c138
biyue <18961736622@163.com>
2019-11-23 23:15:15 +0800

    jdk-Lock学习

---
+++ src/main/java/com/programming/cultivation/concurrent/lock/LockDemo.java
@@ -0,0 +1,39 @@
+package com.programming.cultivation.concurrent.lock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LockDemo {
+
+
+    public static void main(String[] args) throws Exception{
+        Apple apple = new Apple();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    apple.getApple("A");
+                    System.out.println("我是创建出来的线程");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        apple.getApple("B");
+        System.out.println("我是主线程");
+    }
+
+    static class Apple{
+        Lock lock = new ReentrantLock();
+
+        public Apple getApple(String info) throws Exception{
+            System.out.println(info + "Method getApple lock before");
+            lock.lock();
+            Thread.sleep(5000);
+            System.out.println(info + "Method getApple lock after");
+            lock.unlock();
+            return new Apple();
+        }
+
+    }
+}
---
+++ src/test/java/com/programming/cultivation/CultivationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programming.cultivation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CultivationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

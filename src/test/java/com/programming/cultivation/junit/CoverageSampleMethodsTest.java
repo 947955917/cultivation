b96c07a6665add6d08b0c7fee4096b4c2ae11291
biyue <18961736622@163.com>
2020-03-07 14:57:34 +0800

    单元测试练习

--- src/test/java/com/programming/cultivation/junit/CoverageSampleMethodsTest.java
+++ src/test/java/com/programming/cultivation/junit/CoverageSampleMethodsTest.java
@@ -2,6 +2,7 @@ package com.programming.cultivation.junit;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -11,12 +12,25 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class CoverageSampleMethodsTest {
 
     @Test
-    @ParameterizedTest
     @DisplayName("daughter")
-    @CsvSource(value = {"1,2,3","4,5,6"})
+    @RepeatedTest(2)
+    @ParameterizedTest
+    @CsvSource(value = {"1,2,3"})
     public void testCoverageSampleMethods(int k, int j, int d) {
         CoverageSampleMethods methods = new CoverageSampleMethods();
         Assertions.assertTrue(methods.testMethod(k, j, d));
     }
 
+
+    @RepeatedTest(2)
+    public void test3() {
+        CoverageSampleMethods methods = new CoverageSampleMethods();
+        Assertions.assertTrue(methods.testMethod(1,2,3));
+    }
+
+
+    @RepeatedTest(3)
+    public void test(){
+        System.out.println("11");
+    }
 }

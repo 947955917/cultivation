81df54102e8488b40f558e1cc2ded3b69e69bf86
biyue <18961736622@163.com>
2020-03-08 22:31:57 +0800

    java 网络学习，TCP发包代码

---
+++ src/main/java/com/programming/cultivation/jdk/net/tcp/Client.java
@@ -0,0 +1,28 @@
+package com.programming.cultivation.jdk.net.tcp;
+
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+/**
+ * TCP 客户端
+ * 1、发起连接
+ * 2、获取字节输出流
+ * 3、输出字节数据
+ * 4、关闭流
+ * 5、关闭连接
+ */
+public class Client {
+
+    public static void main(String[] args) throws Exception {
+        Socket socket = new Socket("localhost", 9999);
+        System.out.println(socket.getPort());
+        System.out.println(socket.getLocalPort());
+
+//        socket.connect(new InetSocketAddress("localhost",9999));
+        OutputStream outputStream = socket.getOutputStream();
+        outputStream.write("我是客户端".getBytes());
+        socket.close();
+    }
+}
---
+++ src/main/java/com/programming/cultivation/jdk/net/tcp/Server.java
@@ -0,0 +1,33 @@
+package com.programming.cultivation.jdk.net.tcp;
+
+import java.io.InputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * TCP 服务端
+ * 1、建立服务端Socket
+ * 2、监听端口
+ * 3、获取Socket连接
+ * 4、获取IO流
+ * 6、处理数据
+ * 7、关闭连接
+ */
+public class Server {
+
+    public static void main(String[] args) throws Exception {
+        ServerSocket serverSocket = new ServerSocket();
+        serverSocket.bind(new InetSocketAddress("localhost", 9999));
+        while (true) {
+            Socket socket = serverSocket.accept();
+            System.out.println("远程端口：" + socket.getPort());
+            System.out.println("本地端口：" + socket.getLocalPort());
+            InputStream inputStream = socket.getInputStream();
+            byte[] data = new byte[inputStream.available()];
+            inputStream.read(data);
+            System.out.println(new String(data));
+            socket.close();
+        }
+    }
+}
